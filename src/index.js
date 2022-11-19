6fb621fecf3db57fec33798bf688f2dfcb418b76
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-19 16:37:27 -0300

    juntando http e url

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-const url = require('url')
+const http = require('http')
+const server = http.createServer((req, res) => {
+    const url = require('url')
+    const urlInfo = url.parse(req.url, true)
+    const name = urlInfo.query.nome
 
-const decomposedUrl = new url.URL('https://rianvitor.vercel.app')
 
-console.log('href:' + ' ' +  decomposedUrl.href)
-console.log("protocol:" + " " +  decomposedUrl.protocol);
-console.log("host:" + " " +  decomposedUrl.host);
-console.log("hostname:" + " " +  decomposedUrl.hostname);
-console.log("origin:" + " " +  decomposedUrl.origin);
+    res.setHeader('Content-Type', 'text/html; charset=utf8')
+    res.statusCode = 200
 
+
+    if (!name) {
+        res.end(`
+            <h1>Insira seu nome</h1>
+            <form action="GET">
+                <label for="name">Name</label>
+                <input type="text" name="nome">
+                <input type="submit">
+            </form>/        
+        `);
+    } else {
+        res.end(`Hello, ${name}`); 
+    }
+})
+
+const port  = 3002
+server.listen(port, console.log(`http://localhost:${port}`))
