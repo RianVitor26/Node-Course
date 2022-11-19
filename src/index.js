d8a6b5ac0332102413a87efd154cd1ed9c73abb9
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-19 17:59:31 -0300

    unindo http, url e fs

--- src/index.js
+++ src/index.js
@@ -7,11 +7,24 @@ const headerConfig = {
 };
 
 const server = http.createServer((req, res) => {
-  fs.readFile("./index.html", (err, data) => {
-    res.writeHead(200, headerConfig);
-    res.write(data);
-    return res.end();
-  });
+    const urlInfo = require("url").parse(req.url, 200)
+    const name = urlInfo.query.name
+
+
+  if (!name) {
+    fs.readFile("./index.html", (err, data) => {
+      res.writeHead(200, headerConfig);
+      res.write(data);
+      return res.end();
+    });
+  } else {
+      fs.writeFile("tabelaNome.txt", name, (err, data) => {
+          res.writeHead(302, {
+            location:  '/'
+          });
+        return res.end();
+      });
+  }
 });
 
 server.listen(port, console.log(`http://localhost:${port}`));
