3356e116300ec8e736857b87fe008af7f05c99b3
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-19 23:05:30 -0300

    metodo resolve do path core retorna o caminho de algum arquivo

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-const http = require("http");
-const fs = require("fs");
-const port = 3002;
-const headerConfig = {
-    ContentType: "text/html",
-    charset: "utf8"
-};
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-    const urlInfo = require("url").parse(req.url, 200)
-    const name = urlInfo.query.name
-
-
-  if (!name) {
-    fs.readFile("./index.html", (err, data) => {
-      res.writeHead(200, headerConfig);
-      res.write(data);
-      return res.end();
-    });
-  } else {
-      fs.writeFile("tabelaNome.txt", name, (err, data) => {
-          res.writeHead(302, {
-            location:  '/'
-          });
-        return res.end();
-      });
-  }
-});
-
-server.listen(port, console.log(`http://localhost:${port}`));
+console.log(path.resolve('index.js'))
