7bbd455d4add3865565fb6f3e1e06abac88b9d0f
RianVitor26 <rianvitorlhe@gmail.com>
2022-11-19 23:11:15 -0300

    alguns metodos do path

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const path = require('path')
+const url = "www.mercadolivre"
 
 console.log(path.resolve('index.js'))
+console.log(path.basename(url))
+console.log(path.join(url, "/produtos.com"))
+console.log(path.extname('index.js'))
+console.log(__dirname  + '/index.js')
+
