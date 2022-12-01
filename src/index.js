cf1345ee0c91a10c3679bf04affbc2103a6212c2
RianVitor26 <rianvitorlhe@gamil.com>
2022-12-01 00:07:52 -0300

    conectando o node com o Mysql

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const mysql = require("mysql2");
 const port = 3000;
 const server = express();
-const routes = require("./routes");
 
-server.use(express.json());
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "olamysql",
+  database: "teste",
+});
 
-server.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-server.use(routes)
-
-
-server.listen(port, () => console.log(`http://localhost:${port}`));
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Conectou ao Mysql");
+    server.listen(port, () => console.log(`http://localhost:${port}`));
+  }
+});
